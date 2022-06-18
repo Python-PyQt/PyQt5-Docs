88faafadd7586f23bd63ba9584ed8fc8a17ee81a
Phil Thompson <phil@riverbankcomputing.com>
2022-06-18 14:25:16 +0100

    Updated the sip module docs.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copyright = '{0}, Riverbank Computing Limited, The Qt Company'.format(
 # The short X.Y version.
 version = '5.15'
 # The full version, including alpha/beta/rc tags.
-release = version
+release = '5.15.4'
 
 # The number of columns in a table of class names.
 sip_class_table_columns = 4
